b82bcc5c25eaf75729c6f4108122228220c08950
Yuito Murase <yuito@acupof.coffee>
2020-06-24 08:32:50 +0900

    Add regression test for table-of-contesnts page

--- __test__/regression.test.js
+++ __test__/regression.test.js
@@ -5,16 +5,25 @@ expect.extend({ toMatchPdfSnapshot });
 shell.cd('__test__');
 
 afterAll(() => {
-  shell.rm('test.pdf', 'test.satysfi-aux');
+  shell.rm('*test.pdf', '*test.satysfi-aux');
 })
 
 test('Confirm that satysfi is installed', () => {
   expect(shell.exec('satysfi -v').code).toBe(0);
 })
 
-test(`Compile class file`, () => {
-  const code = shell.exec(`satysfi test.saty -o test.pdf`, {silent: true}).code;
+describe(`yabaitech SATySFi class file`, () => {
+  it(`Generates table-of-contents pages as expected`, () => {
+    const code = shell.exec(`satysfi toc.test.saty -o toc.test.pdf`, {silent: true}).code;
+  
+    expect(code).toBe(0);
+    expect('toc.test.pdf').toMatchPdfSnapshot();   
+  });
 
-  expect(code).toBe(0);
-  expect('test.pdf').toMatchPdfSnapshot();
-});
+  it(`Generates the whole pages as expected`, () => {
+    const code = shell.exec(`satysfi test.saty -o test.pdf`, {silent: true}).code;
+  
+    expect(code).toBe(0);
+    expect('test.pdf').toMatchPdfSnapshot();   
+  });
+})
