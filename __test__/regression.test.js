420db75f619075575f84e70641d17202d4f43677
Yuito Murase <yuito@acupof.coffee>
2020-07-07 10:00:07 +0900

    Remove trailing spaces

--- __test__/regression.test.js
+++ __test__/regression.test.js
@@ -15,15 +15,15 @@ test('Confirm that satysfi is installed', () => {
 describe(`yabaitech SATySFi class file`, () => {
   it(`Generates table-of-contents pages as expected`, () => {
     const code = shell.exec(`satysfi toc.test.saty -o toc.test.pdf`, {silent: true}).code;
-  
+
     expect(code).toBe(0);
-    expect('toc.test.pdf').toMatchPdfSnapshot();   
+    expect('toc.test.pdf').toMatchPdfSnapshot();
   });
 
   it(`Generates the whole pages as expected`, () => {
     const code = shell.exec(`satysfi test.saty -o test.pdf`, {silent: true}).code;
-  
+
     expect(code).toBe(0);
-    expect('test.pdf').toMatchPdfSnapshot();   
+    expect('test.pdf').toMatchPdfSnapshot();
   });
 })
