7aad0cbc9505fb9e080b30a96f65352b1a500587
Yuito Murase <yuito@acupof.coffee>
2020-07-24 15:33:43 +0900

    Apply Prettier

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # satysfi-class-yabaitech
+
 [![build](https://github.com/yabaitechtokyo/satysfi-class-yabaitech/workflows/build/badge.svg?branch=master)](https://github.com/yabaitechtokyo/satysfi-class-yabaitech/actions?query=workflow%3Abuild)
 
-[satysfi-class-yabaitech](https://github.com/yabaitechtokyo/satysfi-class-yabaitech) is a SATySFi class file that we use for publishing our magazines [*yabaitech.tokyo*](https://yabaitech.tokyo). It is originally based on [gfngfn](https://github.com/gfngfn)'s [cs-thesis](https://github.com/gfngfn/cs-thesis).
+[satysfi-class-yabaitech](https://github.com/yabaitechtokyo/satysfi-class-yabaitech) is a SATySFi class file that we use for publishing our magazines [_yabaitech.tokyo_](https://yabaitech.tokyo). It is originally based on [gfngfn](https://github.com/gfngfn)'s [cs-thesis](https://github.com/gfngfn/cs-thesis).
 
 ## About yabaitech.tokyo
+
 You can buy our magazines at [BOOTH](https://booth.pm). Check them out!
 
-* [yabaitech.tokyo vol.1](https://booth.pm/ja/items/1041379)
-* [yabaitech.tokyo vol.2](https://booth.pm/ja/items/1304326)
-* [yabaitech.tokyo vol.3](https://booth.pm/ja/items/1557104)
-* [yabaitech.tokyo vol.4](https://booth.pm/ja/items/1888547)
+- [yabaitech.tokyo vol.1](https://booth.pm/ja/items/1041379)
+- [yabaitech.tokyo vol.2](https://booth.pm/ja/items/1304326)
+- [yabaitech.tokyo vol.3](https://booth.pm/ja/items/1557104)
+- [yabaitech.tokyo vol.4](https://booth.pm/ja/items/1888547)
--- __test__/regression.test.js
+++ __test__/regression.test.js
@@ -1,29 +1,34 @@
-const shell = require('shelljs');
-const { toMatchPdfSnapshot } = require('jest-pdf-snapshot');
+const shell = require("shelljs");
+const { toMatchPdfSnapshot } = require("jest-pdf-snapshot");
 expect.extend({ toMatchPdfSnapshot });
 
-shell.cd('__test__');
+shell.cd("__test__");
 
 afterAll(() => {
-  shell.rm('*test.pdf', '*test.satysfi-aux');
-})
+  shell.rm("*test.pdf", "*test.satysfi-aux");
+});
 
-test('Confirm that satysfi is installed', () => {
-  expect(shell.exec('satysfi -v').code).toBe(0);
-})
+test("Confirm that satysfi is installed", () => {
+  expect(shell.exec("satysfi -v").code).toBe(0);
+});
 
 describe(`yabaitech SATySFi class file`, () => {
   it(`Generates table-of-contents pages as expected`, () => {
-    const code = shell.exec(`satysfi toc/toc.test.saty -o toc.test.pdf`, {silent: true}).code;
+    const code = shell.exec(`satysfi toc/toc.test.saty -o toc.test.pdf`, {
+      silent: true,
+    }).code;
 
     expect(code).toBe(0);
-    expect('toc.test.pdf').toMatchPdfSnapshot();
+    expect("toc.test.pdf").toMatchPdfSnapshot();
   });
 
   it(`Generates the whole pages as expected`, () => {
-    const code = shell.exec(`satysfi integration/integration.test.saty -o integration.test.pdf`, {silent: true}).code;
+    const code = shell.exec(
+      `satysfi integration/integration.test.saty -o integration.test.pdf`,
+      { silent: true }
+    ).code;
 
     expect(code).toBe(0);
-    expect('integration.test.pdf').toMatchPdfSnapshot();
+    expect("integration.test.pdf").toMatchPdfSnapshot();
   });
-})
+});
