dfb196b5f79bbd88bcf00f83a767fe1ff4604cb0
Yuito Murase <yuito@acupof.coffee>
2020-07-24 15:57:06 +0900

    Introduce eslint and fix issues

--- __test__/regression.test.js
+++ __test__/regression.test.js
@@ -1,5 +1,7 @@
 const shell = require("shelljs");
+
 const { toMatchPdfSnapshot } = require("jest-pdf-snapshot");
+
 expect.extend({ toMatchPdfSnapshot });
 
 shell.cd("__test__");
@@ -14,19 +16,21 @@ test("Confirm that satysfi is installed", () => {
 
 describe(`yabaitech SATySFi class file`, () => {
   it(`Generates table-of-contents pages as expected`, () => {
-    const code = shell.exec(`satysfi toc/toc.test.saty -o toc.test.pdf`, {
+    const { code } = shell.exec(`satysfi toc/toc.test.saty -o toc.test.pdf`, {
       silent: true,
-    }).code;
+    });
 
     expect(code).toBe(0);
     expect("toc.test.pdf").toMatchPdfSnapshot();
   });
 
   it(`Generates the whole pages as expected`, () => {
-    const code = shell.exec(
+    const {
+      code,
+    } = shell.exec(
       `satysfi integration/integration.test.saty -o integration.test.pdf`,
       { silent: true }
-    ).code;
+    );
 
     expect(code).toBe(0);
     expect("integration.test.pdf").toMatchPdfSnapshot();
